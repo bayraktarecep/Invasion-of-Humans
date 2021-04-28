f92bcb5b8f7946ec0023052ad06f89bf260f87c3
bayraktarecep <bayraktarecep@hotmail.com>
2021-04-28 14:23:49 +0300

    Initial Commit

---
+++ Invasion of Humans/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Invasion of Humans
+//
+//  Created by Recep Bayraktar on 28.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
